0960745a9c38b1d326066960f32923af9a3c0194
ikarashi-r <ikarashi_r@works-hi.co.jp>
2022-03-30 16:43:41 +0900

    Update Hoge

--- src/main/java/spring/practice1/Hoge.java
+++ src/main/java/spring/practice1/Hoge.java
@@ -1,6 +1,7 @@
 package spring.practice1;
 
-import java.util.logging.Logger;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class Hoge {
 	private static final Logger logger = LoggerFactory.getLogger(Hoge.class);
