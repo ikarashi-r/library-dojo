fc55e8324fbcd20ce4dc8fbaa3997858166d14a9
ikarashi-r <ikarashi_r@works-hi.co.jp>
2022-03-30 16:40:56 +0900

    Add spring practice

---
+++ src/main/java/spring/practice1/Hoge.java
@@ -0,0 +1,12 @@
+package spring.practice1;
+
+import java.util.logging.Logger;
+
+public class Hoge {
+	private static final Logger logger = LoggerFactory.getLogger(Hoge.class);
+
+	public void hoge() {
+		logger.info("hoge called");
+	}
+
+}
---
+++ src/main/java/spring/practice1/Main.java
@@ -0,0 +1,10 @@
+package spring.practice1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Hoge hoge = new Hoge();
+		hoge.hoge();
+	}
+
+}
