c3ab65de08b37fcae761d110cfa942f7e7de8ae5
ikarashi-r <ikarashi_r@works-hi.co.jp>
2022-03-30 19:57:12 +0900

    Add ComponentScan practice

---
+++ src/main/java/spring/practice2/HogeCs.java
@@ -0,0 +1,13 @@
+package spring.practice2;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class HogeCs {
+	private static final Logger logger = LoggerFactory.getLogger(HogeCs.class);
+
+	public void hoge() {
+		logger.info("hoge called");
+	}
+
+}
---
+++ src/main/java/spring/practice2/MainCs.java
@@ -0,0 +1,9 @@
+package spring.practice2;
+
+public class MainCs {
+	public static void main(String[] args) {
+		HogeCs hoge = new HogeCs();
+		hoge.hoge();
+	}
+
+}
